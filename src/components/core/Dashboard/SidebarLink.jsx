37b91ff494127bbf051b4159d8770b459ee0020b
Akshay <akshaysabharwal92@gmail.com>
2025-02-13 21:45:48 +0530

    .

--- src/components/core/Dashboard/SidebarLink.jsx
+++ src/components/core/Dashboard/SidebarLink.jsx
@@ -15,18 +15,18 @@ export default function SidebarLink({ link, iconName }) {
       to={link.path}
       onClick={() => dispatch(resetCourseState())}
       className={`relative flex items-center gap-x-3 px-6 py-3 rounded-md text-sm font-medium
-        transition-all duration-200 bg-white shadow-md hover:shadow-lg hover:shadow-black
-        ${matchRoute(link.path) ? "bg-[black] text-white" : "text-richblack-900"}
+        transition-all duration-200 bg-white shadow-sm hover:shadow-md hover:shadow-black
+        ${matchRoute(link.path) ? "bg-yellow-800 text-yellow-50" : "text-gray-700"}
       `}
     >
       {/* Sidebar Indicator */}
       <span
-        className={`absolute left-0 top-0 h-full w-1 bg-[white] transition-all duration-300 ${matchRoute(link.path) ? "opacity-100" : "opacity-0"
+        className={`absolute left-0 top-0 h-full w-1 bg-yellow-500 transition-all duration-300 ${matchRoute(link.path) ? "opacity-100" : "opacity-0"
           }`}
       ></span>
 
       {/* Icon */}
-      <Icon className="text-xl text-richblack-800" />
+      <Icon className="text-xl text-gray-600" />
 
       {/* Link Name */}
       <span>{link.name}</span>
