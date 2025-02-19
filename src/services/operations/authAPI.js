a04b809ed9c297947187eaafef0e549f2f20a7b7
Akshay <akshaysabharwal92@gmail.com>
2025-02-19 11:59:17 +0530

    .

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -7,40 +7,40 @@ import { apiConnector } from "../apiConnector.js"
 import { endpoints } from "../apis.js"
 
 const {
-  // SENDOTP_API,
+  SENDOTP_API,
   SIGNUP_API,
   LOGIN_API,
   RESETPASSTOKEN_API,
   RESETPASSWORD_API,
 } = endpoints
 
-// export function sendOtp(email, navigate) {
-//   return async (dispatch) => {
-//     const toastId = toast.loading("Loading...")
-//     dispatch(setLoading(true))
-//     try {
-//       const response = await apiConnector("POST", SENDOTP_API, {
-//         email,
-//         checkUserPresent: true,
-//       })
-//       // console.log("SENDOTP API RESPONSE............", response)
+export function sendOtp(email, navigate) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    dispatch(setLoading(true))
+    try {
+      const response = await apiConnector("POST", SENDOTP_API, {
+        email,
+        checkUserPresent: true,
+      })
+      // console.log("SENDOTP API RESPONSE............", response)
 
-//       // console.log(response.data.success)
+      // console.log(response.data.success)
 
-//       if (!response.data.success) {
-//         throw new Error(response.data.message)
-//       }
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
 
-//       toast.success("OTP Sent Successfully")
-//       navigate("/verify-email")
-//     } catch (error) {
-//       console.log("SENDOTP API ERROR............", error)
-//       toast.error("Could Not Send OTP")
-//     }
-//     dispatch(setLoading(false))
-//     toast.dismiss(toastId)
-//   }
-// }
+      toast.success("OTP Sent Successfully")
+      navigate("/verify-email")
+    } catch (error) {
+      console.log("SENDOTP API ERROR............", error)
+      toast.error("Could Not Send OTP")
+    }
+    dispatch(setLoading(false))
+    toast.dismiss(toastId)
+  }
+}
 
 export function signUp(
   accountType,
