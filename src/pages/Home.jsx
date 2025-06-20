c7e0fbcda88070dd0ddee7d30ed50f182686ee85
Akshay <akshaysabharwal92@gmail.com>
2025-06-20 19:20:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -227,8 +227,7 @@ const NewsComponent = () => {
   const scrollSpeed = 0.5; // Adjust speed
 
   const API_KEY = process.env.REACT_APP_GUARDIAN_API_KEY;
-  //const URL = `https://content.guardianapis.com/search?api-key=29c22464-e81b-4fc1-a176-decdaabc7f45&section=technology|education&show-fields=thumbnail,headline,trailText`;
-  const URL =  'https://content.guardianapis.com/search?api-key=29c22464-e81b-4fc1-a176-decdaabc7f45';
+  const URL = `https://content.guardianapis.com/search?api-key=29c22464-e81b-4fc1-a176-decdaabc7f45&section=technology|education&show-fields=thumbnail,headline,trailText`;
   useEffect(() => {
     axios
       .get(URL)
